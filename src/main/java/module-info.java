f9b76c795423db96e8fbe86fe187588ca8ce57af
Twisha Chachra <twishachachra@gmail.com>
2022-05-31 14:34:28 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.assignment1gc200474646 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.assignment1gc200474646 to javafx.fxml;
+    exports com.example.assignment1gc200474646;
+}
